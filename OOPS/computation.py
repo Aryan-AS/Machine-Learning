660815c3f6935d51414c543d355bd1378c7207d8
Aryan-AS <89517035+Aryan-AS@users.noreply.github.com>
2024-04-23 18:19:59 +0000

    modified:   OOPS/computation.py

--- OOPS/computation.py
+++ OOPS/computation.py
@@ -1,52 +1,58 @@
 class computer:
     def __init__(self):
-       pass
-    def factorial(self,n):
-       factorial = 1
+        pass
 
-       if n < 0:
-          return "Invalid"
-       elif n == 0:
-          return 1
-       else:
-          for i in range(1,n + 1):
-               factorial = factorial*i
-          return factorial
-    def naturalsum(self,n):
-     sum = 0
-     for i in range(1,n+1):
-          sum = sum + i
-     print(sum)
-    def testPrime(self,n):
-     if n == 1:
+    def factorial(self, n):
+        if n < 0:
+            return "Invalid"
+        elif n == 0:
+            return 1
+        else:
+            factorial = 1
+            for i in range(1, n + 1):
+                factorial *= i
+            return factorial
+
+    def naturalsum(self, n):
+        total = 0
+        for i in range(1, n + 1):
+            total += i
+        return total
+
+    def testPrime(self, n):
+        if n < 2:
+            return "not prime"
+        for i in range(2, int(n ** 0.5) + 1):
+            if n % i == 0:
+                return "not prime"
         return "prime"
-     for i in range(2,n):
-       if n%i==0:
-          return "not prime"
-       else:
-          return "prime"
-    def testprims(self,n1,n2):
-       list1 = []
-       list2 = []
-       for i in range(1,n1):
-          if n1%i==0:
-             list1.append(i)
-       for i in range(1,n2):
-          if n2%i==0:
-             list2.append(i)
-             
-       meow = set(list1,list2)
-       if meow == {1}:
-          return "It is prims"
-       else:
-          return "It is not prims"
-       
+
+    def testprims(self, n1, n2):
+        list1 = [i for i in range(1, n1) if n1 % i == 0]
+        list2 = [i for i in range(1, n2) if n2 % i == 0]
+        
+        common_factors = set(list1).intersection(list2)
+        if common_factors == {1}:
+            return "It is prims"
+        else:
+            return "It is not prims"
+
+    def tableMult(self, n):
+        return [n * i for i in range(1, 11)]
+
+    def listDiv(self, n):
+        listd = [i for i in range(1, n) if n % i == 0]
+        return listd
+
+
 aryan = computer()
-aryan.factorial(6)
-aryan.naturalsum(7)
 
-
-       
+print("Factorial of 6:", aryan.factorial(6))
+print("Sum of natural numbers up to 7:", aryan.naturalsum(7))
+print("Multiplication table of 6:", aryan.tableMult(6))
+print("Is 5 a prime number?", aryan.testPrime(5))
+print("Are 5 and 6 prime with each other?", aryan.testprims(5, 6))
+print("Divisors of 5:", aryan.listDiv(5))
 
     
           
