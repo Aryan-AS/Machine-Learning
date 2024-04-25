8058e178b4183ffe20aff70d9c9a517613daedac
Aryan-AS <89517035+Aryan-AS@users.noreply.github.com>
2024-04-25 18:25:58 +0000

    modified:   OOPS/count.py

--- OOPS/count.py
+++ OOPS/count.py
@@ -1,16 +1,13 @@
-class Point:
-    def __init__(self,x,y):
-        self.x1 = x
-        self.x2 = y
-    def __str__(self):
-        return "<{},{}>".format(self.x1,self.x2)
-    def eculudiandistance(self,other):
-        return ((self.x1-other.x1)**2 + (self.x2-other.x2)**2)**0.5
-    def fromorigin(self):
-        return self.eculudiandistance(Point(0,0))
-    
+class Car:
+    counting = 0
+    def __init__(self):
+        self.meow = Car.counting
+        Car.counting =Car.counting +1
 
-meow = Point(1,2)
-woof = Point(2,4)
-print(meow.eculudiandistance(woof))
-print(meow.fromorigin())
+
+
+
+maruti = Car()
+bmw = Car()
+honda = Car()
+print(Car.counting)
