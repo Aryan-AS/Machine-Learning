afbcc6f8c998d70c6db00475dc96d411325cf527
Aryan-AS <89517035+Aryan-AS@users.noreply.github.com>
2024-05-08 23:09:49 +0530

    Update 1.py

---
+++ FileHandling/1.py
@@ -0,0 +1,19 @@
+with open("first.txt","w") as f:
+    f.write('''An essay is, generally, a piece of writing that gives the author's own argument, but the definition is vague, overlapping with those of a letter, a paper, an article, a pamphlet, and a short story. Essays have been sub-classified as formal and informal: formal essays are characterized by "serious purpose, dignity, logical organization, length," whereas the informal essay is characterized by the personal element (self-revelation, individual tastes and experiences, confidential manner), humor, graceful style, rambling structure, unconventionality or novelty of theme," etc.[1]
+
+Essays are commonly used as literary criticism, political manifestos, learned arguments, observations of daily life, recollections, and reflections of the author. Almost all modern essays are written in prose, but works in verse have been dubbed essays (e.g., Alexander Pope's An Essay on Criticism and An Essay on Man). While brevity usually defines an essay, voluminous works like John Locke's An Essay Concerning Human Understanding and Thomas Malthus's An Essay on the Principle of Population are counterexamples.
+
+In some countries (e.g., the United States and Canada), essays have become a major part of formal education.[2] Secondary students are taught structured essay formats to improve their writing skills; admission essays are often used by universities in selecting applicants, and in the humanities and social sciences essays are often used as a way of assessing the performance of students during final exams''')
+
+f.close()
+
+def get_final_line(a):
+ with open(a,"r") as d:
+  lines = d.readlines()
+  print(lines[-1])
+     
+     
+                     
+                     
+                     
+get_final_line("first.txt")
